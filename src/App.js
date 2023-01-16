58b948c07e8fea92c8d88ad7f7573d0bd66f3b27
lLija <ilija.trokovski@gmail.com>
2023-01-16 01:56:55 +0100

    finished

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React, { useState } from "react";
-import Cart from "./components/Cart/Cart";
+import { useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 import CartProvider from "./store/CartProvider";
 
 function App() {
-  const [cartIsShown, setCartisShown] = useState(false);
+  const [cartIsShown, setCartIsShown] = useState(false);
 
   const showCartHandler = () => {
-    setCartisShown(true);
+    setCartIsShown(true);
   };
 
   const hideCartHandler = () => {
-    setCartisShown(false);
+    setCartIsShown(false);
   };
 
   return (
